66520ebcadc3d1fc9a1992f0575830454c894783
Elliott <elliottbrunet@gmail.com>
2022-10-12 15:41:56 +0200

    merge minting + nft contracts

--- test/Merge.test.ts
+++ test/Merge.test.ts
@@ -1,14 +1,12 @@
 import { ethers, upgrades } from 'hardhat'
 import { expect } from 'chai'
 import { beforeEach } from 'mocha'
-import { FantiumMinterV1 } from '../typechain-types'
 import { FantiumNFTV1 } from '../typechain-types/contracts/FantiumNFTV1'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
 describe("FANtium", () => {
 
     let nftContract: FantiumNFTV1
-    let minterContract: FantiumMinterV1
     let admin: SignerWithAddress
     let fantium: SignerWithAddress
     let athlete: SignerWithAddress
@@ -21,21 +19,13 @@ describe("FANtium", () => {
 
         // Deploy the FantiumNFTV1 contract
         const FantiumNFTV1 = await ethers.getContractFactory("FantiumNFTV1")
-        nftContract = (await FantiumNFTV1.deploy("FANtium", "FAN", 1)) as FantiumNFTV1
-        await nftContract.deployed()
+        nftContract = await upgrades.deployProxy(FantiumNFTV1, ["FANtium", "FAN", 1], { initializer: 'initialize' }) as FantiumNFTV1
+        console.log("Minter proxy address: ", nftContract.address)
 
-        // Deploy the minter contract
-        const FantiumMinterFactory = await ethers.getContractFactory("FantiumMinterV1")
-        minterContract = await upgrades.deployProxy(FantiumMinterFactory, [nftContract.address], { initializer: 'initialize' }) as FantiumMinterV1
-        console.log("Minter proxy address: ", minterContract.address)
+        console.log("Implementation address: ", await upgrades.erc1967.getImplementationAddress(nftContract.address))
 
-        console.log("Implementation address: ", await upgrades.erc1967.getImplementationAddress(minterContract.address))
-
-        await upgrades.upgradeProxy(minterContract, FantiumMinterFactory)
-        console.log("New implemnentation address:", await upgrades.erc1967.getImplementationAddress(minterContract.address))
-
-        // Set the minter contract as the minter for the FantiumNFTV1 contract
-        await nftContract.updateMinterContract(minterContract.address)
+        // await upgrades.upgradeProxy(minterContract, FantiumMinterFactory)
+        // console.log("New implemnentation address:", await upgrades.erc1967.getImplementationAddress(minterContract.address))
 
         // Set Fantium address for primary sale
         await nftContract.updateFantiumPrimarySaleAddress(fantium.address)
@@ -53,7 +43,7 @@ describe("FANtium", () => {
         )
 
         // Add admin to KYC list
-        await minterContract.addAddressToKYC(admin.address);
+        await nftContract.addAddressToKYC(admin.address);
 
         // Unpause collection
         await nftContract.toggleCollectionIsPaused(1)
@@ -65,8 +55,6 @@ describe("FANtium", () => {
     it("Checks if contracts are deployed", async () => {
         expect(await nftContract.name()).to.equal("FANtium")
         expect(await nftContract.symbol()).to.equal("FAN")
-
-        expect(await minterContract.fantium721Address()).to.equal(nftContract.address)
     })
 
     it("Checks if admin can add a collection", async () => {
@@ -75,13 +63,13 @@ describe("FANtium", () => {
     })
 
     it("Checks if admin address is in KYC list", async () => {
-        expect(await minterContract.isAddressKYCed(admin.address)).to.equal(true)
+        expect(await nftContract.isAddressKYCed(admin.address)).to.equal(true)
     })
 
     it("Check if removing address from KYC list disables minting capability", async () => {
-        await minterContract.removeAddressFromKYC(admin.address);
-        expect(await minterContract.isAddressKYCed(admin.address)).to.equal(false)
-        await expect(minterContract.purchaseTo(admin.address, 1, { value: 100 })).to.be.revertedWith("Address not KYCed")
+        await nftContract.removeAddressFromKYC(admin.address);
+        expect(await nftContract.isAddressKYCed(admin.address)).to.equal(false)
+        await expect(nftContract.purchaseTo(admin.address, 1, { value: 100 })).to.be.revertedWith("Address not KYCed")
     })
 
     it("Checks if collection is unpaused", async () => {
@@ -90,19 +78,19 @@ describe("FANtium", () => {
     })
 
     it("Checks if admin can mint a token", async () => {
-        await minterContract.purchaseTo(admin.address, 1, { value: 100 })
+        await nftContract.purchaseTo(admin.address, 1, { value: 100 })
         expect(await nftContract.balanceOf(admin.address)).to.equal(1)
     })
 
     it("Checks that admin cannot mint more than the max supply", async () => {
-        await minterContract.purchaseTo(admin.address, 1, { value: 100 })
+        await nftContract.purchaseTo(admin.address, 1, { value: 100 })
         expect(await nftContract.balanceOf(admin.address)).to.be.revertedWith("Maximum number of invocations reached")
     })
 
     it("Checks if fee is paid to Fantium", async () => {
         const balanceBefore = await fantium.getBalance()
         console.log("Balance before minting:", balanceBefore.toString())
-        await minterContract.purchaseTo(admin.address, 1, { value: 100 })
+        await nftContract.purchaseTo(admin.address, 1, { value: 100 })
         const balanceAfter = await fantium.getBalance()
         console.log("Balance after minting:", balanceAfter.toString())
         expect(balanceAfter.sub(balanceBefore)).to.equal(10)
@@ -111,14 +99,14 @@ describe("FANtium", () => {
     it("Checks if fee is paid to athlete", async () => {
         const balanceBefore = await athlete.getBalance()
         console.log("Balance before minting:", balanceBefore.toString())
-        await minterContract.purchaseTo(admin.address, 1, { value: 100 })
+        await nftContract.purchaseTo(admin.address, 1, { value: 100 })
         const balanceAfter = await athlete.getBalance()
         console.log("Balance after minting:", balanceAfter.toString())
         expect(balanceAfter.sub(balanceBefore)).to.equal(90)
     })
 
     it("Checks if token URI is correct", async () => {
-        await minterContract.purchaseTo(admin.address, 1, { value: 100 })
+        await nftContract.purchaseTo(admin.address, 1, { value: 100 })
         const tokenURI = await nftContract.tokenURI(1000001)
         console.log("Token URI:", tokenURI)
 
