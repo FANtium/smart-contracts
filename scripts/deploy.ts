dbe5ac06a4f64c1b80b654a5201f2069c4fdc598
Elliott <elliottbrunet@gmail.com>
2022-12-23 11:23:44 +0100

    add price in unit calculation

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,15 +12,15 @@ async function main() {
   const mock20 = await Mock20.deploy();
   await mock20.deployed();
 
-  const FantiumNFTV1 = await ethers.getContractFactory("FantiumNFTV1");
-  const nftContract = await upgrades.deployProxy(FantiumNFTV1, ["FANtium", "FAN", deployer.address], { initializer: 'initialize', kind: 'uups'})
-  await nftContract.deployed();
+  // const FantiumNFTV1 = await ethers.getContractFactory("FantiumNFTV1");
+  // const nftContract = await upgrades.deployProxy(FantiumNFTV1, ["FANtium", "FAN", deployer.address], { initializer: 'initialize', kind: 'uups'})
+  // await nftContract.deployed();
 
-  console.log("FantiumNFTV1 deployed to:", nftContract.address);
+  // console.log("FantiumNFTV1 deployed to:", nftContract.address);
 
   const data = {
-    "nftProxy": nftContract.address,
-    "nftImplementation": await upgrades.erc1967.getImplementationAddress(nftContract.address),
+    // "nftProxy": nftContract.address,
+    // "nftImplementation": await upgrades.erc1967.getImplementationAddress(nftContract.address),
     "mock20": mock20.address
   }
   writeFileSync(join(__dirname, './contractAddresses.json'), JSON.stringify(data), {
