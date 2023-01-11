a032c94c2432dbdc6ad5ae4e032a6b60ca569b90
Elliott <elliottbrunet@gmail.com>
2023-01-11 01:03:51 +0100

    wip

---
+++ contracts/FantiumBase.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity 0.8.13;
+
+import "./FantiumNFT.sol";
+
+contract FantiumBase is FantiumNFT {
+    //add gap to avoid storage overlap with FantiumNFT
+    uint256 public newInt; 
+    uint256 public anotherNewInt;
+    uint256[999] private __gap;
+}
