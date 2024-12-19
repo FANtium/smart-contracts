d793105ba0683fed948558417084fbc440aaf6b4
Mathieu Bour <mat@fantium.com>
2024-12-19 10:43:24 +0100

    feat: deploy v5.0.0-alpha.6

--- script/UpgradeTestnet.s.sol
+++ script/UpgradeTestnet.s.sol
@@ -12,11 +12,11 @@ contract DeployTestnetV5 is Script {
     address public constant BACKEND_SIGNER = 0xCAFE914D4886B50edD339eee2BdB5d2350fdC809;
     address public constant DEPLOYER = 0xC0DE5408A46402B7Bd13678A43318c64E2c31EAA;
 
-    bool public FANTIUM_NFT_UPGRADE = false;
+    bool public FANTIUM_NFT_UPGRADE = true;
     address public constant FANTIUM_NFT_PROXY = 0x4d09f47fd98196CDFC816be9e84Fb15bCDB92612;
     bool public FANTIUM_USER_MANAGER_UPGRADE = false;
     address public constant FANTIUM_USER_MANAGER_PROXY = 0x54dF3fb8B090A3FBf583e29e8fBd388A0179F4A2;
-    bool public FANTIUM_CLAIMING_UPGRADE = true;
+    bool public FANTIUM_CLAIMING_UPGRADE = false;
     address public constant FANTIUM_CLAIMING_PROXY = 0xB578fb2A0BC49892806DC7309Dbe809f23F4682F;
 
     function run() public {
