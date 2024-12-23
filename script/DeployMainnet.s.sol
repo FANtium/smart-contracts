a65656383490bf491221bc19f7d1d48491c1c78b
Mathieu Bour <mat@fantium.com>
2024-12-23 10:57:05 +0100

    feat: deploy FANtiumNFTV6 on testnet

--- script/DeployMainnet.s.sol
+++ script/DeployMainnet.s.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import { Upgrades } from "@openzeppelin/foundry-upgrades/LegacyUpgrades.sol";
 import { Script } from "forge-std/Script.sol";
-import { FANtiumNFTV6 } from "src/FANtiumNFTV6.sol";
+import { Core } from "@openzeppelin/foundry-upgrades/internal/Core.sol";
+import { Options } from "@openzeppelin/foundry-upgrades/Options.sol";
 
 contract DeployMainnetV5 is Script {
-    address private constant FANTIUM_NFT_PROXY = 0x2b98132E7cfd88C5D854d64f436372838A9BA49d;
-    address private constant FANTIUM_NFT_ADMIN = 0x1111111111111111111111111111111111111111;
-
     function run() public {
-        Upgrades.upgradeProxy(
-            FANTIUM_NFT_PROXY, "FANtiumNFTV6.sol", abi.encodeCall(FANtiumNFTV6.initialize, (FANTIUM_NFT_ADMIN))
-        );
+        vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+        Options memory opts;
+        Core.prepareUpgrade("FANtiumNFTV6.sol", opts);
+        vm.stopBroadcast();
     }
 }
