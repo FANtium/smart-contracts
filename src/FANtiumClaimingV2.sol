cf85e42a35c57873c39d7160dbdec977d6ef4b6b
Mathieu Bour <mat@fantium.com>
2024-12-27 16:47:04 +0100

    feat: implement ERC2771 for UserManager and Claiming contracts

--- src/FANtiumClaimingV2.sol
+++ src/FANtiumClaimingV2.sol
@@ -220,6 +220,33 @@ contract FANtiumClaimingV2 is
     }
 
     // ========================================================================
+    // ERC2771
+    // ========================================================================
+    function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
+        return hasRole(FORWARDER_ROLE, forwarder);
+    }
+
+    function _msgSender() internal view virtual override returns (address sender) {
+        if (isTrustedForwarder(msg.sender)) {
+            // The assembly code is more direct than the Solidity version using `abi.decode`.
+            /// @solidity memory-safe-assembly
+            assembly {
+                sender := shr(96, calldataload(sub(calldatasize(), 20)))
+            }
+        } else {
+            return super._msgSender();
+        }
+    }
+
+    function _msgData() internal view virtual override returns (bytes calldata) {
+        if (isTrustedForwarder(msg.sender)) {
+            return msg.data[:msg.data.length - 20];
+        } else {
+            return super._msgData();
+        }
+    }
+
+    // ========================================================================
     // Setters
     // ========================================================================
     function setFANtiumNFT(IFANtiumNFT _fantiumNFT) external onlyManagerOrAdmin {
