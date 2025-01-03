c09d2316d5402acfa951166eeff849af3b741710
Mathieu Bour <mat@fantium.com>
2025-01-03 17:44:00 +0100

    feat: deploy v6.0.0

--- script/DeployMainnetV6.s.sol
+++ script/DeployMainnetV6.s.sol
@@ -5,11 +5,14 @@ import { Script } from "forge-std/Script.sol";
 import { Core } from "@openzeppelin/foundry-upgrades/internal/Core.sol";
 import { Options } from "@openzeppelin/foundry-upgrades/Options.sol";
 
-contract DeployMainnetV5 is Script {
+contract DeployMainnetV6 is Script {
     function run() public {
         vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+
         Options memory opts;
         Core.prepareUpgrade("FANtiumNFTV6.sol", opts);
+        Core.prepareUpgrade("FANtiumClaimingV2.sol", opts);
+        Core.prepareUpgrade("FANtiumUserManagerV2.sol", opts);
         vm.stopBroadcast();
     }
 }
