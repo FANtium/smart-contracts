c52621ec843e7d49cf5717ae0f397129b8325245
Mathieu Bour <mat@fantium.com>
2025-02-14 11:05:07 +0100

    release: v7.0.0

--- script/DeployMainnetV7.s.sol
+++ script/DeployMainnetV7.s.sol
@@ -5,14 +5,22 @@ import { Options } from "@openzeppelin/foundry-upgrades/Options.sol";
 import { Core } from "@openzeppelin/foundry-upgrades/internal/Core.sol";
 import { Script } from "forge-std/Script.sol";
 
-contract DeployMainnetV6 is Script {
+contract DeployMainnetV7 is Script {
+    error OnlyPolygonMainnet();
+
     function run() public {
+        if (block.chainid != 137) {
+            revert OnlyPolygonMainnet();
+        }
+
+        vm.createSelectFork(vm.rpcUrl("polygon"));
         vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
 
         Options memory opts;
-        Core.prepareUpgrade("FANtiumNFTV6.sol", opts);
-        // Core.prepareUpgrade("FANtiumClaimingV2.sol", opts);
+        Core.prepareUpgrade("FANtiumNFTV7.sol", opts);
+        Core.prepareUpgrade("FANtiumClaimingV2.sol", opts);
         // Core.prepareUpgrade("FANtiumUserManagerV2.sol", opts);
+
         vm.stopBroadcast();
     }
 }
