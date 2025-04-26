d0686dd02a44327ae817882d1769a92653a2e839
Mathieu Bour <mat@fantium.com>
2025-04-26 09:43:00 +0200

    feat: update README and add scripts for upgrading FANtiumAthletes, UserManager, and Claiming contracts to new versions

--- script/UpgradeMainnetV9.s.sol
+++ script/UpgradeMainnetV9.s.sol
@@ -5,7 +5,7 @@ import { Options } from "@openzeppelin/foundry-upgrades/Options.sol";
 import { Core } from "@openzeppelin/foundry-upgrades/internal/Core.sol";
 import { Script } from "forge-std/Script.sol";
 
-contract DeployMainnetV8 is Script {
+contract UpgradeMainnetV9 is Script {
     error OnlyPolygonMainnet();
 
     function run() public {
@@ -17,7 +17,9 @@ contract DeployMainnetV8 is Script {
         vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
 
         Options memory opts;
-        Core.prepareUpgrade("FANtiumNFTV8.sol", opts);
+        Core.prepareUpgrade("FANtiumAthletesV9.sol:FANtiumAthletesV9", opts);
+        Core.prepareUpgrade("FANtiumUserManagerV4.sol:FANtiumUserManagerV4", opts);
+        Core.prepareUpgrade("FANtiumClaimingV3.sol:FANtiumClaimingV3", opts);
         vm.stopBroadcast();
     }
 }
